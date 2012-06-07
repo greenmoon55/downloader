808a62ecc945f866f13c3278d31f7ed948e9a26c
greenmoon55 <greenmoon55@ubuntu>
2012-06-07 14:15:29 +0800

    参考网上的代码，写了注释，大家看看吧。

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 downloader
 ==========
+2012.6.7 
+	http://kunalmaemo.blogspot.com/2011/07/simple-download-manager-with-pause.html
+	对这里的代码稍微改动一点，加了注释，大家按程序执行的过程看就好了，不难。加油写啊～
---
+++ downloadmanager.cpp
@@ -0,0 +1,98 @@
+#include "downloadmanager.h"
+
+#include <QDebug>
+
+
+// DownloadManager 继承自 QObject，调用parent的析构函数后这个也会消失
+DownloadManager::DownloadManager(QObject *parent) :
+    QObject(parent),mCurrentReply(0),mFile(0),mDownloadSizeAtPause(0)
+{
+    mManager = new QNetworkAccessManager(this);
+}
+
+void DownloadManager::download( QUrl url)
+{
+    qDebug() << "download: URL=" <<url.toString();
+
+    mDownloadSizeAtPause =0;
+    mCurrentRequest = QNetworkRequest(url);
+    mFile = new QFile("download.part");
+    mFile->open(QIODevice::ReadWrite);
+
+    download(mCurrentRequest);
+}
+
+void DownloadManager::download( QNetworkRequest& request)
+{
+    qDebug() << "download( QNetworkRequest& request )";
+    mCurrentReply = mManager->get(request);
+
+    // 这三个信号都是QNetworkReply自带的信号，都不用我们去写。
+    connect(mCurrentReply,SIGNAL(finished()),this,SLOT(finished()));
+    connect(mCurrentReply,SIGNAL(downloadProgress(qint64,qint64)),this,SLOT(downloadProgress(qint64,qint64)));
+    connect(mCurrentReply,SIGNAL(error(QNetworkReply::NetworkError)),this,SLOT(error(QNetworkReply::NetworkError)));
+}
+
+void DownloadManager::pause()
+{
+    qDebug() << "pause()";
+    if(mCurrentReply == 0) {
+        return;
+    }
+
+    // disconnect the signal/slot so we don't get any error signal and mess with our error handling code
+    // and most importantly it write data to our IODevice here its file which is storing downloaded data.
+    disconnect(mCurrentReply,SIGNAL(finished()),this,SLOT(finished()));
+    disconnect(mCurrentReply,SIGNAL(downloadProgress(qint64,qint64)),this,SLOT(downloadProgress(qint64,qint64)));
+    disconnect(mCurrentReply,SIGNAL(error(QNetworkReply::NetworkError)),this,SLOT(error(QNetworkReply::NetworkError)));
+
+    mCurrentReply->abort();
+    mFile->write(mCurrentReply->readAll());
+
+    mCurrentReply->deleteLater(); // 我加的
+    mCurrentReply = 0;
+}
+
+void DownloadManager::resume()
+{
+    qDebug() << "resume()";
+
+    mDownloadSizeAtPause = mFile->size();
+
+    // Http Header 里放上范围信息，http://en.wikipedia.org/wiki/List_of_HTTP_header_fields
+    QByteArray rangeHeaderValue = "bytes=" + QByteArray::number(mDownloadSizeAtPause) + "-";
+    mCurrentRequest.setRawHeader("Range",rangeHeaderValue);
+
+    download(mCurrentRequest);
+}
+
+
+
+void DownloadManager::finished()
+{
+    qDebug() << "finished";
+    mFile->close();
+    mFile->rename("download.part","download.complete");
+    mFile = 0;
+
+    mCurrentReply->deleteLater();
+
+    mCurrentReply = 0;
+    emit downloadComplete();
+}
+
+// bytesTotal indicates the total number of bytes expected to be downloaded
+void DownloadManager::downloadProgress (qint64 bytesReceived, qint64 bytesTotal)
+{
+    qDebug() << "Download Progress: Received=" << mDownloadSizeAtPause+bytesReceived <<": Total=" << mDownloadSizeAtPause+bytesTotal;
+
+    mFile->write( mCurrentReply->readAll() );
+    int percentage = ((mDownloadSizeAtPause+bytesReceived) * 100 )/ (mDownloadSizeAtPause+bytesTotal);
+    qDebug() << percentage;
+    emit progress(percentage);
+}
+
+void DownloadManager::error(QNetworkReply::NetworkError code)
+{
+    qDebug() << "Error:"<<code;
+}
